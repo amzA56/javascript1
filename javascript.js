6b34752b9d9bee22206885b165f44c0fe139adcb
aptech <aptech@gmail.com>
2023-09-04 14:01:02 +0500

    javacsript

---
+++ javascript.js
@@ -0,0 +1,29 @@
+var sum1 = prompt("kuch bh likho");
+var sum2 = prompt("kuch bh likh lo");
+
+ var sum =5*4;
+
+let sum = (sum1 , sum2) => {
+    return ` ${sum1} ${sum2}` 
+}
+console.log(sum1*sum2)
+
+
+// let name = "Aptech gdn"
+// let age = 67
+
+// var obj1 = {
+//     name,
+//     age
+// }
+// console.log(obj1)
+
+// var sum1 = 3;
+// var sum2 = 4;
+// let = sum ( sum1 , sum2) =>{
+//     return sum;
+// }
+
+
+
+
